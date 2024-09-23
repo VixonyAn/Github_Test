a7da0bfa3f5a3c5c86db93a01e2fe793a93eef4d
VixonyAn <vixonyan@gmail.com>
2024-09-23 12:52:09 +0200

    new class apple

---
+++ Github_Test/Apple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Test
+{
+    internal class Apple
+    {
+        #region Instance Fields
+        private string _apple;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
