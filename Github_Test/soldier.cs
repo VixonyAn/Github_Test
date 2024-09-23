7a29081c16a4364ca1ee8f4f29c39eb102574765
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2024-09-23 13:02:59 +0200

    regions

--- Github_Test/soldier.cs
+++ Github_Test/soldier.cs
@@ -8,13 +8,21 @@ namespace Github_Test
 {
     internal class soldier
     {
+        #region instance fields
+
         private string _name;
         private int _age;
         private int _hp;
 
+        #endregion
+
+        #region Properties
+
         public string Name { get { return _name; } }
         public int Age { get { return _age; } }
         public int HP { get { return _hp; } }
+
+        #endregion
 
     }
 }
