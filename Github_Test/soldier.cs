b6a0b777b90b398fbc076c6fd55a093284e70555
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2024-09-23 13:00:38 +0200

    Ændret class

---
+++ Github_Test/soldier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Test
+{
+    internal class soldier
+    {
+        private string _name;
+        private int _age;
+        private int _hp;
+
+        public string Name { get { return _name; } }
+        public int Age { get { return _age; } }
+        public int HP { get { return _hp; } }
+
+    }
+}
