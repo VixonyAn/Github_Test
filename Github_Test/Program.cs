23ea165f5a91753f054345acba9d6788465663c6
VixonyAn <vixonyan@gmail.com>
2024-09-23 11:35:27 +0200

    ny linje

--- Github_Test/Program.cs
+++ Github_Test/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dette er mit program.");
+
+Console.WriteLine("Jeg har tilføjet noget");
